51e2f7b90cdd07adb11f286eddeb0d127aedfcc3
EC2 Default User <ec2-user@ip-172-31-8-43.eu-west-1.compute.internal>
2025-04-06 20:14:09 +0000

    azure commit

--- weather_event_planner/settings.py
+++ weather_event_planner/settings.py
@@ -121,6 +121,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
